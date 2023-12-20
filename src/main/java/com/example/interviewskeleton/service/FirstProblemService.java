a58a7923dfa4157e19c87c708bf8651b848beff5
Andrei Dumitrescu <andreidumi2405@gmail.com>
2023-12-20 13:44:21 +0200

    Completion of Task 1: Improve Performance

--- src/main/java/com/example/interviewskeleton/service/FirstProblemService.java
+++ src/main/java/com/example/interviewskeleton/service/FirstProblemService.java
@@ -1,6 +1,7 @@
 package com.example.interviewskeleton.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +11,15 @@ import java.util.stream.Collectors;
 
 @Service
 @Slf4j
+@EnableAsync
 public class FirstProblemService {
     private static final String FORBIDDEN_CHARACTER = "a";
 
     public void saveWords(List<String> words) {
-        words.forEach(word -> {
-            if (word.contains(FORBIDDEN_CHARACTER)) {
-                throw new UnsupportedOperationException("This word is not valid!");
-            }
-
-            saveWordToExternalApi(word);
-        });
+        words.stream()
+                .filter(word -> !containsForbiddenCharacter(word))
+                .map(this::callToMethodOfExternal)
+                .collect(Collectors.toList());
     }
 
     private void saveWordToExternalApi(String word) {
@@ -30,5 +29,17 @@ public class FirstProblemService {
         } catch (Throwable anyException) {
             log.info("[BEST EFFORT] Saving word '{}' failed: {}", word, anyException.getMessage());
         }
+    }
+
+    @Async
+    public CompletableFuture<Void> callToMethodOfExternal(String word) {
+        return CompletableFuture.runAsync(() -> saveWordToExternalApi(word));
+    }
+
+    private static boolean containsForbiddenCharacter(String word) {
+        if (word.contains(FORBIDDEN_CHARACTER)) {
+            throw new UnsupportedOperationException("This word is not valid!");
+        }
+        return false;
     }
 }
